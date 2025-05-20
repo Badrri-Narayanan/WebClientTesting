2037b0fa1d65df5829cf97e60408a80c6ab9f054
Badrri Narayanan S <badrri1995@gmail.com>
2025-05-20 02:12:46 +0000

    add initial spring files

---
+++ src/test/java/com/badrri/webclienttesting/WebClientTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badrri.webclienttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
